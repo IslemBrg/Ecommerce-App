dcc71198834999f5e5acddf81fdf29777ad9ef75
IslemBrg <Islem.bargaoui@istic.ucar.tn>
2021-12-28 21:30:40 +0100

    Ordering Css Files

--- eommerce-app/src/App.js
+++ eommerce-app/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './components/ResponsiveAppBar'
 import SignUp from './components/SignUp'
 import HomePage from './components/HomePage'
+import {BrouserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <HomePage></HomePage>
+      </Router>
     </div>
   );
 }
