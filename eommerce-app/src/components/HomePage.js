5888375593024a9932c40de2ccdd7155841cc2d9
mohamed lwed <mohamed.elwed@esen.u-manouba.tn>
2021-12-28 15:49:40 +0100

    v2

--- eommerce-app/src/components/HomePage.js
+++ eommerce-app/src/components/HomePage.js
@@ -1,10 +1,14 @@
+import { Button } from '@mui/material';
 import React from 'react'
 import Logo from '../img/logo.png';
+import '../home.css';
 
 export default function HomePage() {
     return (
-        <div styles="padding:200vw">
-            <img src={Logo} styles={padding="200vw"}/>
+        <div className="home">
+            <img src={Logo} width="60%"/>
+            <Button>Sign in</Button>
+            <Button>Sign up</Button>
         </div>
     )
 }
