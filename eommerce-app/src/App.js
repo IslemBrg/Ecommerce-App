77f7dac20527f252981790f07432c377be811775
IslemBrg <Islem.bargaoui@istic.ucar.tn>
2021-12-28 23:50:56 +0100

    homepage Update

--- eommerce-app/src/App.js
+++ eommerce-app/src/App.js
@@ -1,13 +1,10 @@
 import './CSS/App.css';
-import ResponsiveAppBar from './components/ResponsiveAppBar'
-import SignUp from './components/SignUp'
 import HomePage from './components/HomePage'
-//import {BrouserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <HomePage/>
     </div>
   );
 }
--- eommerce-app/src/components/HomePage.js
+++ eommerce-app/src/components/HomePage.js
@@ -2,11 +2,7 @@ import { Button } from '@mui/material';
 import Logo from '../img/logoYell.png';
 import '../CSS/home.css';
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+
 
 export default function HomePage() {
     return (
